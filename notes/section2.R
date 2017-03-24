f845b14b86b4148c433f0f09a9e91cd03d556ece
jpkunkler <pascal.kunkler@gmail.com>
2017-03-24 16:24:46 +0100

    Added notes folder and notes for section 2

---
+++ notes/section2.R
@@ -0,0 +1,12 @@
+setwd("C:/Users/Jan Pascal Kunkler/git/R-Programming-Advanced/datasets/")
+getwd()
+fin <- read.csv('Future-500.csv')
+
+# Take a first look at the data
+head(fin)
+str(fin)
+summary(fin)
+
+# convert columns to factor
+fin$ID <- factor(fin$ID)
+fin$Inception <- factor(fin$Inception)
