7f2eb4c0acaca7e11f177e3f442a7a9643bd95fa
jpkunkler <pascal.kunkler@gmail.com>
2017-03-28 12:42:24 +0200

    Used gsub to clean up data

--- notes/section2.R
+++ notes/section2.R
@@ -12,3 +12,27 @@ fin$ID <- factor(fin$ID)
 fin$Inception <- factor(fin$Inception)
 
 #Factor Variable Trap (FVT)
+
+# As factor
+fin$Profit <- factor(fin$Profit)
+
+# As numeric
+fin$Profit <- as.numeric(as.character(fin$Profit))
+
+# sub() and gsub()
+# used to replace characters, symbols etc in datasets
+fin$Expenses <- gsub(" Dollars", "", fin$Expenses)
+fin$Expenses <- gsub(",", "", fin$Expenses)
+
+fin$Revenue <- gsub("\\$", "", fin$Revenue)
+fin$Revenue <- gsub(",", "", fin$Revenue)
+head(fin)
+
+fin$Growth <- gsub("%", "", fin$Growth)
+
+str(fin) # <-- we see that we already have character variables
+fin$Expenses <- as.numeric(fin$Expenses) 
+fin$Revenue <- as.numeric(fin$Revenue)
+fin$Growth <- as.numeric(fin$Growth)
+
+summary(fin) # <-- all 3 columns are recognized and analyzed as numeric. Success!
