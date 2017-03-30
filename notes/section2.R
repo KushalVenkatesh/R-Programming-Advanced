4a8231b59774f469a2ccebdbee984a69832b389f
jpkunkler <pascal.kunkler@gmail.com>
2017-03-30 10:35:13 +0200

    Added section 2 code

--- notes/section2.R
+++ notes/section2.R
@@ -44,3 +44,71 @@ complete.cases(fin) # check for all rows which are complete <-- contain NO NA VA
 
 # subset with inversed logic
 fin[!complete.cases(fin),]
+
+# Create backup
+fin_backup <- fin
+
+
+fin <- fin[!is.na(fin$Industry),]
+
+# Reset index
+rownames(fin) <- NULL
+
+# Check missing states
+fin[is.na(fin$State),]
+
+# Set State for New York City
+fin[is.na(fin$State) & fin$City == "New York", "State"] <- "NY"
+
+# Set state for San Francisco
+fin[is.na(fin$State) & fin$City == "San Francisco", "State"] <- "CA"
+
+# Check for missing Employees Values
+fin[is.na(fin$Employees),]
+
+#------------------- MEDIAN IMPUTATION METHOD
+# Check overall median
+median(fin$Employees, na.rm = T)
+
+# Check mean for comparison
+mean(fin$Employees, na.rm = T)
+
+# Calculate Retail Industry Employees median
+med_ret_emp <- median(fin[fin$Industry == "Retail", "Employees"], na.rm = T)
+fin[is.na(fin$Employees) & fin$Industry == "Retail", "Employees"] <- med_ret_emp
+
+# Calculate Financial Services Employees median
+med_finserv_emp <- median(fin[fin$Industry == "Financial Services", "Employees"], na.rm = T)
+fin[is.na(fin$Employees) & fin$Industry == "Financial Services", "Employees"] <- med_finserv_emp
+
+fin[!complete.cases(fin),]
+
+# ------------------------ CONSTRUCTION INDUSTRY
+
+# imputate revenue values for Construction industry
+med_con_rev <- median(fin[fin$Industry == "Construction", "Revenue"], na.rm = T)
+fin[is.na(fin$Revenue) & fin$Industry == "Construction" & is.na(fin$Profit), "Revenue"] <- med_con_rev
+
+# imputate revenue values for Construction industry
+med_con_rev <- median(fin[fin$Industry == "Construction", "Revenue"], na.rm = T)
+fin[is.na(fin$Revenue) & fin$Industry == "Construction" & is.na(fin$Profit), "Revenue"] <- med_con_rev
+
+# imputate profit values for Construction industry
+med_con_prof <- median(fin[fin$Industry == "Construction", "Profit"], na.rm = T)
+fin[is.na(fin$Profit) & fin$Industry == "Construction" & is.na(fin$Expenses), "Profit"] <- med_con_prof
+
+# Calculate Expenses as Revenue - Profit
+fin[fin$Industry == "Construction" & is.na(fin$Expenses), "Expenses"] <- fin[fin$Industry == "Construction" & is.na(fin$Expenses), "Revenue"] - fin[fin$Industry == "Construction" & is.na(fin$Expenses), "Profit"]
+
+# Imputate Growth for Construction Industry
+fin[is.na(fin$Growth),"Growth"] <- median(fin[fin$Industry == "Construction","Growth"], na.rm = T)
+
+# Check if any missing data is left
+fin[!complete.cases(fin),]
+
+# Only one case left to be calculated. Let's do it.
+fin[is.na(fin$Expenses), "Expenses"] <- fin[is.na(fin$Expenses), "Revenue"] - fin[is.na(fin$Expenses), "Profit"]
+
+#--------------------------- VISUALIZATION
+# Now that our data is cleaned up, we are ready to visualize it as requested!
+
