0ee8a51474ba3b43f1013c0cbb39775c13320349
Jan Pascal Kunkler <Jan Pascal Kunkler>
2017-04-03 15:42:40 +0200

    Working with timestamps and rearranging dataframes

--- notes/section3.R
+++ notes/section3.R
@@ -7,6 +7,14 @@ head(util,12)
 str(util)
 summary(util)
 
+# Derive Utilization column
 util$Utilization <- 1 - util$Percent.Idle
-
 summary(util)
+
+# Handling date-times in R
+# POSIX = Portable Operating System Interface
+util$PosixTime <- as.POSIXct(util$Timestamp, format="%d/%m/%Y %H:%M")
+util$Timestamp <- NULL
+
+# rearrange columns
+util <- util[,c(4,1,2,3)]
