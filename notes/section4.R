8a1c143fd740804934d490db14b7c06cf529f4b1
Jan Pascal Kunkler <Jan Pascal Kunkler>
2017-04-05 14:33:45 +0200

    Used apply() and lapply() functions

--- notes/section4.R
+++ notes/section4.R
@@ -29,3 +29,36 @@ Weather
 Weather$NewYork
 Weather[[2]]
 
+# using apply()
+apply(Chicago, 1, mean)
+# check if correct:
+mean(Chicago["DaysWithPrecip",])
+
+# analyze one city:
+Chicago
+apply(Chicago, 1, min)
+apply(Chicago, 1, max)
+
+
+# compare averages
+apply(Chicago, 1, mean)
+apply(NewYork, 1, mean)
+apply(Houston, 1, mean)
+apply(SanFrancisco, 1, mean)
+                      # <<< (nearly) deliv 1: but there is a faster way!
+
+# use lapply() to apply function to a list (or vector)
+lapply(Weather, t) # transpose every single matrix in our list
+
+#example 2
+rbind(Chicago, NewRow=1:12) # Add a new row
+lapply(Weather, rbind, NewRow=1:12)
+
+#example 3
+?rowMeans
+
+lapply(Weather, rowMeans)
+                      # <<< (nearly) deliv 1: even better, but will improve further!
+
+
+ 
